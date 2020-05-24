3da84705701e17ab7868aaee4e8d5801d678c8d4
Furkan Yavuz <furkan@bigarmor.com>
2020-05-24 07:17:51 +0300

    organize imports

--- basicInfoServer.ts
+++ basicInfoServer.ts
@@ -2,13 +2,10 @@
  * @description holds server main
  */
 import dotenv from 'dotenv'
+import cors from 'cors';
+import { Routes } from './app/routes';
 import express = require('express');
 import bodyParser = require('body-parser');
-import cors from 'cors';
-import { context } from './app/context';
-import { Routes } from './app/routes';
-import { ResponseCode } from './app/models/Constant';
-import { Request, Response } from 'express';
 
 dotenv.config();
 
