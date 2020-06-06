b92fc60d21d2bf7b2e7e65990591de9dd8ac63fb
Fatih Turker <fatihturker35@gmail.com>
2020-06-06 13:38:41 +0300

    Implementing response encryption

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -3,9 +3,28 @@ import productRouter from './productRoute';
 import { Request, Response } from 'express';
 import { context } from '../context';
 import { handle } from '../services/errorHandler';
+import { EncryptionService } from '../services/encryptionService';
 
 export module Routes {
  export function mount(app) {
+
+const responseInterceptor = (req, res, next) => {
+   var originalSend = res.send;
+   const service = new EncryptionService();
+   res.send = function(){
+      console.log("Starting Encryption: ", new Date());
+      let encrypted_arguments = service.encrypt(arguments);
+      console.log("Encryption Completed: ", new Date());
+
+      originalSend.apply(res, encrypted_arguments);
+   };
+
+   next();
+   }
+   
+   // use this interceptor before routes
+   app.use(responseInterceptor);
+   
 
   // INFO: Keep this method at top at all times
   app.all('/*', async (req: Request, res: Response, next) => {
