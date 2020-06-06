1dff59a9a580838360a8cd3b5eef5520577487fb
Fatih Turker <fatihturker35@gmail.com>
2020-06-06 13:42:48 +0300

    Fix indentation

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -6,49 +6,49 @@ import { handle } from '../services/errorHandler';
 import { EncryptionService } from '../services/encryptionService';
 
 export module Routes {
- export function mount(app) {
+  export function mount(app) {
 
-const responseInterceptor = (req, res, next) => {
-   var originalSend = res.send;
-   const service = new EncryptionService();
-   res.send = function(){
-      console.log("Starting Encryption: ", new Date());
-      let encrypted_arguments = service.encrypt(arguments);
-      console.log("Encryption Completed: ", new Date());
+    const responseInterceptor = (req, res, next) => {
+      var originalSend = res.send;
+      const service = new EncryptionService();
+      res.send = function () {
+        console.log("Starting Encryption: ", new Date());
+        let encrypted_arguments = service.encrypt(arguments);
+        console.log("Encryption Completed: ", new Date());
 
-      originalSend.apply(res, encrypted_arguments);
-   };
-
-   next();
-   }
-   
-   // use this interceptor before routes
-   app.use(responseInterceptor);
-   
-
-  // INFO: Keep this method at top at all times
-  app.all('/*', async (req: Request, res: Response, next) => {
-   try {
-      // create context
-      res.locals.ctx = await context(req);
+        originalSend.apply(res, encrypted_arguments);
+      };
 
       next();
-   } catch (e) {
-      let error = handle(e);
-      res.status(error.code).send({message: error.message});
-   }
-  });
+    }
 
-  app.use('/user', userRouter);
-  app.use('/product', productRouter);
+    // use this interceptor before routes
+    app.use(responseInterceptor);
 
-  // Use for error handling
-  app.use(function (err, req, res, next) {
-    let error = handle(err);
-    console.log(err);
-    res.status(error.code).send({message: error.message});
-  });
 
- }
+    // INFO: Keep this method at top at all times
+    app.all('/*', async (req: Request, res: Response, next) => {
+      try {
+        // create context
+        res.locals.ctx = await context(req);
+
+        next();
+      } catch (e) {
+        let error = handle(e);
+        res.status(error.code).send({ message: error.message });
+      }
+    });
+
+    app.use('/user', userRouter);
+    app.use('/product', productRouter);
+
+    // Use for error handling
+    app.use(function (err, req, res, next) {
+      let error = handle(err);
+      console.log(err);
+      res.status(error.code).send({ message: error.message });
+    });
+
+  }
 }
 
