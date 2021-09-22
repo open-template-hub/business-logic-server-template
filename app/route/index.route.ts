000cb348b45a86db6e5133127271b8b7ef9a204d
Fatih Turker <fatihturker35@gmail.com>
2021-09-22 18:52:00 +0300

    Introduce MQ

--- app/route/index.route.ts
+++ app/route/index.route.ts
@@ -7,14 +7,26 @@ import {
   DebugLogUtil,
   EncryptionUtil,
   ErrorHandlerUtil,
+  MessageQueueProvider,
   MongoDbProvider,
   PreloadUtil,
 } from '@open-template-hub/common';
 import { NextFunction, Request, Response } from 'express';
 import { Environment } from '../../environment';
-import { publicRoutes as monitorPublicRoutes, router as monitorRouter, } from './monitor.route';
-import { adminRoutes as productAdminRoutes, router as productRouter, } from './product.route';
-import { adminRoutes as userAdminRoutes, publicRoutes as userPublicRoutes, router as userRouter, } from './user.route';
+import { BasicInfoQueueConsumer } from '../consumer/basic-info-queue.consumer';
+import {
+  publicRoutes as monitorPublicRoutes,
+  router as monitorRouter,
+} from './monitor.route';
+import {
+  adminRoutes as productAdminRoutes,
+  router as productRouter,
+} from './product.route';
+import {
+  adminRoutes as userAdminRoutes,
+  publicRoutes as userPublicRoutes,
+  router as userRouter,
+} from './user.route';
 
 const subRoutes = {
   root: '/',
@@ -25,98 +37,112 @@ const subRoutes = {
 
 export namespace Routes {
   let mongodb_provider: MongoDbProvider;
+  var message_queue_provider: MessageQueueProvider;
   let environment: Environment;
   const errorHandlerUtil = new ErrorHandlerUtil();
   const debugLogUtil = new DebugLogUtil();
   let publicRoutes: string[] = [];
   let adminRoutes: string[] = [];
 
-  function populateRoutes( mainRoute: string, routes: Array<string> ) {
+  function populateRoutes(mainRoute: string, routes: Array<string>) {
     let populated = Array<string>();
-    for ( const s of routes ) {
-      populated.push( mainRoute + ( s === '/' ? '' : s ) );
+    for (const s of routes) {
+      populated.push(mainRoute + (s === '/' ? '' : s));
     }
 
     return populated;
   }
 
-  export const mount = ( app: any ) => {
+  export const mount = (app: any) => {
     environment = new Environment();
-    mongodb_provider = new MongoDbProvider( environment.args() );
+    mongodb_provider = new MongoDbProvider(environment.args());
     const preloadUtil = new PreloadUtil();
 
+    message_queue_provider = new MessageQueueProvider(environment.args());
+
+    const channelTag = new Environment().args().mqArgs
+      ?.basicInfoServerMessageQueueChannel as string;
+    message_queue_provider.getChannel(channelTag).then((channel: any) => {
+      const basicInfoQueueConsumer = new BasicInfoQueueConsumer(channel);
+      message_queue_provider.consume(
+        channel,
+        channelTag,
+        basicInfoQueueConsumer.onMessage,
+        1
+      );
+    });
+
     preloadUtil
-    .preload( mongodb_provider )
-    .then( () => console.log( 'DB preload is completed.' ) );
+      .preload(mongodb_provider)
+      .then(() => console.log('DB preload is completed.'));
 
     publicRoutes = [
-      ...populateRoutes( subRoutes.monitor, monitorPublicRoutes ),
-      ...populateRoutes( subRoutes.user, userPublicRoutes ),
+      ...populateRoutes(subRoutes.monitor, monitorPublicRoutes),
+      ...populateRoutes(subRoutes.user, userPublicRoutes),
     ];
-    console.log( 'Public Routes: ', publicRoutes );
+    console.log('Public Routes: ', publicRoutes);
 
     adminRoutes = [
-      ...populateRoutes( subRoutes.product, productAdminRoutes ),
-      ...populateRoutes( subRoutes.user, userAdminRoutes ),
+      ...populateRoutes(subRoutes.product, productAdminRoutes),
+      ...populateRoutes(subRoutes.user, userAdminRoutes),
     ];
-    console.log( 'Admin Routes: ', adminRoutes );
+    console.log('Admin Routes: ', adminRoutes);
 
     // create response interceptor
     const responseInterceptor = (
-        req: Request,
-        res: Response,
-        next: NextFunction
+      req: Request,
+      res: Response,
+      next: NextFunction
     ) => {
       let originalSend = res.send;
-      const encryptionUtil = new EncryptionUtil( environment.args() );
+      const encryptionUtil = new EncryptionUtil(environment.args());
       res.send = function () {
-        debugLogUtil.log( 'Starting Encryption: ', new Date() );
-        let encrypted_arguments = encryptionUtil.encrypt( arguments );
-        debugLogUtil.log( 'Encryption Completed: ', new Date() );
+        debugLogUtil.log('Starting Encryption: ', new Date());
+        let encrypted_arguments = encryptionUtil.encrypt(arguments);
+        debugLogUtil.log('Encryption Completed: ', new Date());
 
-        originalSend.apply( res, encrypted_arguments as any );
+        originalSend.apply(res, encrypted_arguments as any);
       } as any;
 
       next();
     };
 
     // Use this interceptor before routes
-    app.use( responseInterceptor );
+    app.use(responseInterceptor);
 
     // INFO: Keep this method at top at all times
-    app.all( '/*', async ( req: Request, res: Response, next: NextFunction ) => {
+    app.all('/*', async (req: Request, res: Response, next: NextFunction) => {
       try {
         // create context
         res.locals.ctx = await context(
-            req,
-            environment.args(),
-            publicRoutes,
-            adminRoutes,
-            mongodb_provider
+          req,
+          environment.args(),
+          publicRoutes,
+          adminRoutes,
+          mongodb_provider
         );
 
         next();
-      } catch ( err ) {
-        let error = errorHandlerUtil.handle( err );
-        res.status( error.code ).json( { message: error.message } );
+      } catch (err) {
+        let error = errorHandlerUtil.handle(err);
+        res.status(error.code).json({ message: error.message });
       }
-    } );
+    });
 
     // INFO: Add your routes here
-    app.use( subRoutes.monitor, monitorRouter );
-    app.use( subRoutes.user, userRouter );
-    app.use( subRoutes.product, productRouter );
+    app.use(subRoutes.monitor, monitorRouter);
+    app.use(subRoutes.user, userRouter);
+    app.use(subRoutes.product, productRouter);
 
     // Use for error handling
-    app.use( function (
-        err: Error,
-        req: Request,
-        res: Response,
-        next: NextFunction
+    app.use(function (
+      err: Error,
+      req: Request,
+      res: Response,
+      next: NextFunction
     ) {
-      let error = errorHandlerUtil.handle( err );
-      res.status( error.code ).json( { message: error.message } );
-    } );
+      let error = errorHandlerUtil.handle(err);
+      res.status(error.code).json({ message: error.message });
+    });
   };
 }
-
