ae9f7f78281e1837731333b2192fff5036de2cd9
fatihturker <fatihturker35@gmail.com>
2022-03-12 19:39:53 +0300

    Upgrade Common

--- app/consumer/business-logic-queue.consumer.ts
+++ app/consumer/business-logic-queue.consumer.ts
@@ -1,12 +1,18 @@
-import { BusinessLogicActionType } from '@open-template-hub/common';
+import {
+  BusinessLogicActionType,
+  ContextArgs,
+  QueueConsumer,
+} from '@open-template-hub/common';
 
-export class BusinessLogicQueueConsumer {
+export class BusinessLogicQueueConsumer implements QueueConsumer {
   private channel: any;
+  private ctxArgs: ContextArgs = {} as ContextArgs;
 
   constructor() {}
 
-  init = (channel: string) => {
+  init = (channel: string, ctxArgs: ContextArgs) => {
     this.channel = channel;
+    this.ctxArgs = ctxArgs;
     return this;
   };
 
