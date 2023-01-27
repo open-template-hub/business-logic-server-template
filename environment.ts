d2ddb1380d42dd0a0abecef7ca543d0dc0283968
Fatih Turker <fatihturker35@gmail.com>
2023-01-27 10:03:18 +0300

    Update environment

--- environment.ts
+++ environment.ts
@@ -1,7 +1,7 @@
 import { DbArgs, EnvArgs, MqArgs, TokenArgs } from '@open-template-hub/common';
 
 export class Environment {
-  constructor( private _args: EnvArgs = {} as EnvArgs ) {
+  constructor(private _args: EnvArgs = {} as EnvArgs) {
     const tokenArgs = {
       accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
       responseEncryptionSecret: process.env.RESPONSE_ENCRYPTION_SECRET,
@@ -10,14 +10,16 @@ export class Environment {
     const dbArgs = {
       mongoDbConnectionLimit: process.env.MONGODB_CONNECTION_LIMIT,
       mongoDbUri: process.env.MONGODB_URI,
+      redisUri: process.env.REDISCLOUD_URL,
+      redisConnectionLimit: process.env.REDIS_CONNECTION_LIMIT,
     } as DbArgs;
 
     const mqArgs = {
       messageQueueConnectionUrl: process.env.CLOUDAMQP_URL,
       businessLogicServerMessageQueueChannel:
-      process.env.BUSINESS_LOGIC_SERVER_QUEUE_CHANNEL,
+        process.env.BUSINESS_LOGIC_SERVER_QUEUE_CHANNEL,
       orchestrationServerMessageQueueChannel:
-      process.env.ORCHESTRATION_SERVER_QUEUE_CHANNEL,
+        process.env.ORCHESTRATION_SERVER_QUEUE_CHANNEL,
     } as MqArgs;
 
     this._args = {
